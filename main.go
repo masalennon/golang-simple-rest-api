c1e72bbb05120ab4dd35e264757158cbed230531
masalennon <miyazaki.masanari1@gmail.com>
2019-09-23 23:00:39 +0900

    today's study

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 )
 
 func main() {
+	// mux := http.NewServeMux() //デフォルトのマルチプレクサを生成するコード、リクエストをハンドラにリダイレクトする。
+    // mux.HandleFunc("/", homeLink)
+    // mux.HandleFunc("/events", getAllEvents)
+    // mux.Handle("/events/", http.StripPrefix("/events/", http.HandlerFunc(getOneEvent)))
 
-	http.HandleFunc("/trust", handleTrustRequest)
+	http.HandleFunc("/trust", handleTrustRequest) //第一引数のURLへのリクエストが到着すると、第二引数に指定されているハンドラ関数にリダイレクトされる。全てのハンドラ関数が第一引数にResponseWriterをとり、第二引数にRequestへのポインタを取るのでハンドラ関数に改めて引数を渡す必要はない
+	//ハンドラ関数とは第一引数にResponseWriterをとり、第二引数にRequestへのポインタを取るGoの関数に過ぎない
+	//HTTPリクエスト発生時に呼び出される具体的な処理が書かれています。
 	http.HandleFunc("/belief", handleBeliefRequest)
-	http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8080", nil) //なぜnilでいい？
 }
 
 func handleTrustRequest(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +72,8 @@ var beliefs = allBeliefs{
 
 func handleTrustGet(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("GET")
-	json.NewEncoder(w).Encode(trusts)
+	fmt.Println(r.URL.Path)
+	json.NewEncoder(w).Encode(trusts) //JSONにエンコードしたtrustsをレスポンスボディ（w）に書き込む
 }
 
 func handleBeliefGet(w http.ResponseWriter, r *http.Request) {
